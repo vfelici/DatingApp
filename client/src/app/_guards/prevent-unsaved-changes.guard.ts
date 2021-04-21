8d7b77292b2d1aa0ca7cd8bce685c0a2a30018f3
Valerio Felici <v.felici@cpsweb.it>
2021-04-21 17:40:19 +0200

    fine della sezione 10

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  canDeactivate(component: MemberEditComponent): boolean {
+    if (component.editForm.dirty) {
+      return confirm("Sicuro di voler continuare? Le modifiche non salvate saranno perse")
+    }
+    return true;
+  }
+  
+}
